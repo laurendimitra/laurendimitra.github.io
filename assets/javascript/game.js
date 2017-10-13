c1cef5b2ac861c4381e68a98224864df776ec5d8
laurendimitra <lauren.mooore@bluetruckstudios.co>
2017-10-13 15:27:27 -0400

    bug fix 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,7 +28,7 @@ for (var i = 0; i < numberOptions.length; i++) {
 
   var imageCrystal = $("<img>");
   imageCrystal.addClass("crystal-image");
-  imageCrystal.attr("src", "../images/bluemeth.jpg");
+  imageCrystal.attr("src", "../week-4-game-master/assets/images/bluemeth.jpg");
 
   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
