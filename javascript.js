b5d199aca700cb2739d620d8b91afef99fbae54f
laurendimitra <lauren.mooore@bluetruckstudios.co>
2017-12-06 13:49:46 -0500

    map styling

--- javascript.js
+++ javascript.js
@@ -16,28 +16,67 @@ $(document).ready(function () {
             Materialize.showStaggeredList($(el));
         }
     }];
-    //   var options = [{
-    //     selector: '#staggered-test2',
-    //     offset: 300,
-    //     callback: function(el) {
-    //       Materialize.showStaggeredList($(el));
-    //     }
-    //   }];
 
     Materialize.scrollFire(options);
 
-    function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
 
+    google.maps.event.addDomListener(window, 'load', init);
+
+    function init() {
+
+        var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(43.071755, -70.762553),
+            styles: [{
+                    "featureType": "all",
+                    "elementType": "all",
+                    "stylers": [{
+                            "hue": "#311B92"
+                        },
+                        {
+                            "saturation": 0
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry",
+                    "stylers": [{
+                            "visibility": "simplified"
+                        },
+                        {
+                            "lightness": 100
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                }
+            ]
+        };
+        function pinSymbol(color) {
+            return {
+                path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+                fillColor: color,
+                fillOpacity: 1,
+                strokeColor: '#fff',
+                strokeWeight: 2,
+                scale: 1,
+           };
+        }
+        var mapElement = document.getElementById('map');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(43.071755, -70.762553),
+            map: map,
+            icon: pinSymbol("#000"),
+            title: 'Find Me Here!'
+        });
+    }
 
 
 });
